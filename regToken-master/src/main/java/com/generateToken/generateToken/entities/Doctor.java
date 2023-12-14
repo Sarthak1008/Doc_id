a9981be2d5cc28efc038b456a8d035e294077d9d
Sarthak Aggarwal <sarthak10082001@gmail.com>
2023-12-14 11:18:53 +0530

    first commit

---
+++ regToken-master/src/main/java/com/generateToken/generateToken/entities/Doctor.java
@@ -0,0 +1,61 @@
+package com.generateToken.generateToken.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String specialization;
+    private String degree;
+    private int experience;
+    private String research_journal;
+    private String citations;
+//    @Column(unique = true,length=10)
+//    private String contact;
+
+  @Column(unique = true, length = 10)
+  @Size(min = 10, max = 10, message = "Contact length must be exactly 10 characters")
+  private String contact;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Clinic> clinics = new ArrayList<>();
+
+    public void addClinic(Clinic clinic) {
+        this.clinics.add(clinic);
+    }
+
+    @OneToMany(mappedBy = "doctor",cascade = CascadeType.ALL)
+    private List<Appointment> appointmentPatientList = new ArrayList<>();
+
+
+    public void addAppointmentPatient1(Appointment appointment){
+        this.appointmentPatientList.add(appointment);
+    }
+
+}
---
+++ regToken-master/src/main/java/com/generateToken/generateToken/services/Impl/ClinicServiceImpl.java
@@ -0,0 +1,184 @@
+package com.generateToken.generateToken.services.Impl;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.generateToken.generateToken.dto.AppointmentDTOs;
+import com.generateToken.generateToken.dto.ClinicDto;
+import com.generateToken.generateToken.dto.DoctorDTO;
+import com.generateToken.generateToken.entities.Appointment;
+import com.generateToken.generateToken.entities.Clinic;
+import com.generateToken.generateToken.entities.Doctor;
+import com.generateToken.generateToken.repositories.AppointmentRepository;
+import com.generateToken.generateToken.repositories.ClinicRepository;
+import com.generateToken.generateToken.repositories.DoctorRepository;
+import com.generateToken.generateToken.services.ClinicService;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ClinicServiceImpl implements ClinicService {
+    @Autowired
+    private ClinicRepository clinicRepository;
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public List<AppointmentDTOs> getAppointments(Long clinicId) {
+
+        Clinic clinic = clinicRepository.findById(clinicId)
+                .orElseThrow(() -> new EntityNotFoundException("Clinic not found"));
+
+        return clinic.getAppointmentDto();
+    }
+
+    @Override
+    public List<AppointmentDTOs> getAppointmentBetweenDate(Long clinicId, LocalDate startDate, LocalDate endDate) {
+        Clinic clinic = clinicRepository.findById(clinicId)
+                .orElseThrow(() -> new EntityNotFoundException("Clinic not found"));
+
+        List<AppointmentDTOs> appointments = clinic.getAppointmentDto();
+
+        return appointments.stream()
+                .filter(appointment -> {
+                    LocalDate appointmentDate = appointment.getAppointmentDate();
+                    return !appointmentDate.isBefore(startDate) && !appointmentDate.isAfter(endDate);
+                })
+                .collect(Collectors.toList());
+//        List<AppointmentDTOs> appointmentDTOs1 = new ArrayList<>();
+//        for(AppointmentDTOs appointmentDTOs : appointments){
+//            if(!appointmentDTOs.getAppointmentDate().isBefore(startDate) && !appointmentDTOs.getAppointmentDate().isAfter(endDate)){
+//                appointmentDTOs1.add(appointmentDTOs);
+//            }
+//        }
+//        return appointmentDTOs1;
+    }
+
+    @Override
+    public Double findAmountForClinicInDateRange(Long clinicId, java.util.Date startDate, java.util.Date endDate) {
+        Optional<Clinic> clinicOptional = clinicRepository.findById(clinicId);
+        System.out.println(appointmentRepository.findByAppointmentDateBetween(clinicId,startDate,endDate));
+        if (clinicOptional.isPresent()) {
+            Clinic clinic = clinicOptional.get();
+
+            List<Appointment> clinicAppointments = clinic.getAppointmentList();
+            double totalAmount = 0.0;
+
+            for (Appointment appointment : clinicAppointments) {
+                Date appointmentDate = Date.valueOf(appointment.getAppointmentDate());
+                if (appointmentDate.after(startDate) && appointmentDate.before(endDate)) {
+                    totalAmount += clinic.getFees();
+                }
+            }
+            return totalAmount;
+        } else {
+            return null;
+        }
+    }
+
+    public ClinicDto addClinic(Long doctorId, ClinicDto clinicDto) {
+
+        Doctor doctor = doctorRepository.findById(doctorId)
+                .orElseThrow(() -> new EntityNotFoundException("Doctor not found"));
+
+        DoctorDTO doctorDTO = new DoctorDTO();
+
+        Clinic clinic = new Clinic();
+        clinic.setLocation(clinicDto.getLocation());
+        clinic.setIncharge(clinicDto.getIncharge());
+        clinic.setFees(clinicDto.getFees());
+        clinic.setStartTime(clinicDto.getStartTime());
+        clinic.setEndTime(clinicDto.getEndTime());
+        clinic.setDoctor(doctor);
+        clinic = clinicRepository.save(clinic);
+
+        ClinicDto clinicDto1 = new ClinicDto();
+        clinicDto1.setLocation(clinic.getLocation());
+        clinicDto1.setIncharge(clinic.getIncharge());
+        clinicDto1.setFees(clinic.getFees());
+        clinicDto1.setStartTime(clinic.getStartTime());
+        clinicDto1.setEndTime(clinic.getEndTime());
+
+        doctor.addClinic(clinic);
+
+        //  clinic = clinicRepository.save(clinic);
+        doctor = doctorRepository.save(doctor);
+
+        return clinicDto1;
+    }
+
+    public Optional<Clinic> getClinicById(Long clinicId) {
+        return clinicRepository.findById(clinicId);
+    }
+
+    @Override
+    public String deleteClinic(Long doctor_id, Long clinic_id) {
+        Doctor doctor = doctorRepository.findById(doctor_id)
+                .orElseThrow(() -> new EntityNotFoundException("Doctor not found"));
+
+        Clinic clinicToDelete = clinicRepository.findById(clinic_id)
+                .orElseThrow(() -> new EntityNotFoundException("Clinic not found"));
+
+        // Check if the clinic belongs to the specified doctor
+        if (doctor.getClinics().removeIf(clinic -> clinic.getId().equals(clinic_id))) {
+            // Remove the clinic from the doctor's list and update the relationship
+            doctorRepository.save(doctor);
+            return "Clinic Deleted " + clinic_id + " for Doctor " + doctor_id;
+        } else {
+            return "Clinic not found for deletion for Doctor " + doctor_id;
+        }
+    }
+
+    
+            //System.out.println(clinics.size());
+            
+            // Clinic clinic = clinicRepository.findById(id)
+            //         .orElseThrow(() -> new EntityNotFoundException("Clinic not found"));
+
+            // if (!doctor.getClinics().contains(clinic)) {
+            //     throw new IllegalArgumentException("Clinic does not belong to the specified doctor");
+            // }else{
+            //     String s = "Clinic Deleted "+ id;
+            //     clinicRepository.deleteById(id);
+            //     return s;
+            // }
+        
+
+
+    @Override
+    public ClinicDto updateClinic(Long id, ClinicDto updatedClinicDto) {
+        Clinic clinic = clinicRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Clinic not found"));
+
+        // Update the clinic properties with the values from updatedClinicDto
+        clinic.setLocation(updatedClinicDto.getLocation());
+        clinic.setIncharge(updatedClinicDto.getIncharge());
+        clinic.setFees(updatedClinicDto.getFees());
+        clinic.setStartTime(updatedClinicDto.getStartTime());
+        clinic.setEndTime(updatedClinicDto.getEndTime());
+
+        // Save the updated clinic entity
+        clinic = clinicRepository.save(clinic);
+
+        // Convert the updated clinic entity to ClinicDto
+        ClinicDto updatedClinicDtoResponse = new ClinicDto();
+        updatedClinicDtoResponse.setLocation(clinic.getLocation());
+        updatedClinicDtoResponse.setIncharge(clinic.getIncharge());
+        updatedClinicDtoResponse.setFees(clinic.getFees());
+        updatedClinicDtoResponse.setStartTime(clinic.getStartTime());
+        updatedClinicDtoResponse.setEndTime(clinic.getEndTime());
+
+        return updatedClinicDtoResponse;
+    }
+
+
+}
