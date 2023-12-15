119466f32ecca8644938fe49033ebe6fb3a2ba7e
Sarthak Aggarwal <sarthak10082001@gmail.com>
2023-12-15 14:22:39 +0530

    fifth  commit

---
+++ regToken-master/src/main/java/com/generateToken/generateToken/dto/PrescriptionDto.java
@@ -0,0 +1,39 @@
+package com.generateToken.generateToken.dto;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+
+import com.generateToken.generateToken.Gender.Gender;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrescriptionDto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long prescriptionId;
+    private Long clinicId;
+    private LocalDate currDate;
+    private String doctorName;
+    private Long patientId;
+    private int patientAge;
+    private Gender gender;
+    private List<HashMap<String, Integer>> medicinesPrescribed;
+    private LocalDate startDateOfMedication;
+    private LocalDate endDateOfMedication;
+    private String contactOfClinic;
+    private String speciality;
+    private String location;
+
+    // Constructors, getters, and setters...
+
+}
